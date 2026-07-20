b7ac35c67edf1a108a2ceab6bc04c941637bd767
Fannareme Aboubacar <fannareme.abdou@ashesi.edu.gh>
2026-07-20 20:55:22 +0100

    Sprint 8: Add dark mode toggle

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const themeButton = document.getElementById("theme-toggle");
+
+themeButton.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+        themeButton.textContent = "☀️";
+
+    } else {
+
+        themeButton.textContent = "🌙";
+
+    }
+
+});
